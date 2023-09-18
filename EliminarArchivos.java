3fe6fd308c751f237b67a51b66ed7159f8dd7bfd
Janett Urbano <49128886+jannegcode@users.noreply.github.com>
2023-09-18 11:46:38 -0600

    Update EliminarArchivos.java

--- EliminarArchivos.java
+++ EliminarArchivos.java
@@ -1,1 +1,23 @@
+import java.io.File;
 
+public class EliminarArchivos {
+  
+    public static void deleteDirectory(File directory) {
+        for (File file: Objects.requireNonNull(directory.listFiles())) {
+            if (file.isDirectory()) {
+                deleteDirectory(file);
+            } else {
+                if(file.delete()){
+                	System.out.println("Archivo borrado: " + file.getName());
+                }
+            }
+        }
+    }
+    
+	public static void cleanDirectory(String path){
+        File directory = new File(path);
+		deleteDirectory(directory);
+	}
+
+
+}
