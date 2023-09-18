ef713e21daccdd663c0427f946de29d5399f8559
Janett Urbano <49128886+jannegcode@users.noreply.github.com>
2023-09-18 11:43:58 -0600

    Update FuncionesGenerales.java

--- FuncionesGenerales.java
+++ FuncionesGenerales.java
@@ -1,4 +1,51 @@
 import java.io.File;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.xml.bind.DatatypeConverter;
+
+import json.org.JSONArray;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.joda.time.LocalDate;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.itextpdf.text.pdf.codec.Base64;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class FuncionesGenerales {
   
     public static void deleteDirectory(File directory) {
@@ -64,7 +111,44 @@ public class FuncionesGenerales {
 	        return base64File;
 	}
 	
+	// Extraer archivos ZIP
+	public static boolean unzip(String ZipFilePath, String DestFilePath) throws IOException {
+		
+		boolean extract = false;
+        File Destination_Directory = new File(DestFilePath);
+        if (!Destination_Directory.exists()) {
+        	Destination_Directory.mkdir();
+        }
+        ZipInputStream Zip_Input_Stream = new ZipInputStream(new FileInputStream(ZipFilePath));
+        ZipEntry Zip_Entry = Zip_Input_Stream.getNextEntry();
 
+        while (Zip_Entry != null) {
+            String File_Path = DestFilePath + File.separator + Zip_Entry.getName();
+            if (!Zip_Entry.isDirectory()) {
+            	extract = true;
+                extractFile(Zip_Input_Stream, File_Path);
+            } else {
 
+                File directory = new File(File_Path);
+                directory.mkdirs();
+            }
+            Zip_Input_Stream.closeEntry();
+            Zip_Entry = Zip_Input_Stream.getNextEntry();
+        }
+        Zip_Input_Stream.close();
+        
+        return extract;
+    }
 
+    private static void extractFile(ZipInputStream Zip_Input_Stream, String File_Path) throws IOException {
+        BufferedOutputStream Buffered_Output_Stream = new BufferedOutputStream(new FileOutputStream(File_Path));
+        byte[] Bytes = new byte[BUFFER_SIZE];
+        int Read_Byte = 0;
+        while ((Read_Byte = Zip_Input_Stream.read(Bytes)) != -1) {
+        	Buffered_Output_Stream.write(Bytes, 0, Read_Byte);
+        }
+        Buffered_Output_Stream.close();
+    }
+
+	
 }
